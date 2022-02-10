c12b49dd57bd2e2304abc77628eec85cca52b961
Dipak@1432 <chavandipakr17@gmail.com>
2022-02-10 18:04:50 +0530

    clear email samples

--- UserRegistration/UserRegistration/RegistrationValidation.cs
+++ UserRegistration/UserRegistration/RegistrationValidation.cs
@@ -27,11 +27,12 @@ namespace UserRegistration
         }
         public void Checkemail(string Emailid)
         {
-            string emailpattern = "^[a-z][a-zA-Z0-9._-]{3,20}@[a-z]{3,10}.(com)$";
+            // string emailpattern = "^[a-z][a-zA-Z0-9._-]{3,20}@[a-z]{3,10}.(com)$";
+            string emailpattern = "^[a-zA-Z0-9]+[._+-]{0,1}[0-9a-zA-Z]+[@][0-9a-zA-Z]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2,3}){0,1}$";
             if (Regex.IsMatch(Emailid, emailpattern))
                 Console.WriteLine("Email id is valid");
             else
-                Console.WriteLine("Invalid name!!");
+                Console.WriteLine("Invalid Email id!!");
         }
         public void Checkmobilenum(string Mobilenum)
         {
@@ -43,10 +44,10 @@ namespace UserRegistration
         }
         public void Checkpassward(string Passward)
         {
-            string passwardpattern = "^[A-Za-z0-9]{8,20}$";
-            string passwardpattern1 = "^(?=.*[A-Z])(?=.*[a-z]{8,}).*$";
-            string passwardpattern2 = "^(?=.*[A-Z])(?=.*[0-9]).{8,}$";
-            string passwardpattern3 = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[&%$#@^*!~]).{8,}$";
+           // string passwardpattern = "^[A-Za-z0-9]{8,20}$";
+           // string passwardpattern1 = "^(?=.*[A-Z])(?=.*[a-z]{8,}).*$";
+            //string passwardpattern2 = "^(?=.*[A-Z])(?=.*[0-9]).{8,}$";
+            string passwardpattern = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[&%$#@^*!~]).{8,}$";
             if (Regex.IsMatch(Passward, passwardpattern))
                 Console.WriteLine("Passward is valid");
             else
