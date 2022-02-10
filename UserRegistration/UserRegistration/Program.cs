f366233c4e497a8fbcdf26e0bccf2800f39a208d
Dipak@1432 <chavandipakr17@gmail.com>
2022-02-10 17:22:22 +0530

    Validate last name

--- UserRegistration/UserRegistration/Program.cs
+++ UserRegistration/UserRegistration/Program.cs
@@ -14,7 +14,7 @@ namespace UserRegistration
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("Enter Program number to get executed \n1. Validate first name \n2. Exit");
+                Console.WriteLine("Enter Program number to get executed \n1. Validate first name \n2. Validate last name \n3. Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -23,10 +23,18 @@ namespace UserRegistration
                         Console.WriteLine("*length should be Minimum three characters.");
                         Console.WriteLine("Enter First name :");
                         string fname = Console.ReadLine();
-                        RegistrationValidation Checkpattern = new RegistrationValidation();
+                         RegistrationValidation Checkpattern = new RegistrationValidation();
                         Checkpattern.Checkfirstname(fname);
                         break;
                     case 2:
+                        Console.WriteLine("*First letter should be capital.");
+                        Console.WriteLine("*length should be Minimum three characters.");
+                        Console.WriteLine("Enter Last name :");
+                        string lname = Console.ReadLine();
+                        RegistrationValidation lnamepattern = new RegistrationValidation();
+                        lnamepattern.Checklastname(lname);
+                        break;
+                    case 3:
                         flag = false;
                         break;
                     default:
--- UserRegistration/UserRegistration/RegistrationValidation.cs
+++ UserRegistration/UserRegistration/RegistrationValidation.cs
@@ -11,9 +11,17 @@ namespace UserRegistration
     {
         public void Checkfirstname(string firstname)
         {
-            string Firstnamepattern = "^[A-Z][a-z]{2}$";
+            string Firstnamepattern = "^[A-Z][a-z]{3,10}$";
             if (Regex.IsMatch(firstname, Firstnamepattern))
                 Console.WriteLine("First Name is valid");
+            else
+                Console.WriteLine("Invalid name!!");
+        }
+        public void Checklastname(string Lastname)
+        {
+            string lastnamepattern = "^[A-Z][a-z]{3,10}$";
+            if (Regex.IsMatch(Lastname, lastnamepattern))
+                Console.WriteLine("Last Name is valid");
             else
                 Console.WriteLine("Invalid name!!");
         }
