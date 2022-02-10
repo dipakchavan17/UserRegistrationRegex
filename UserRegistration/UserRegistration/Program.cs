eebce9f9fccf76162fc0c76df3d5092f412baffc
Dipak@1432 <chavandipakr17@gmail.com>
2022-02-10 17:17:15 +0530

    validate first name

--- UserRegistration/UserRegistration/Program.cs
+++ UserRegistration/UserRegistration/Program.cs
@@ -11,6 +11,29 @@ namespace UserRegistration
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to User Registration");
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Enter Program number to get executed \n1. Validate first name \n2. Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("*First letter should be capital.");
+                        Console.WriteLine("*length should be Minimum three characters.");
+                        Console.WriteLine("Enter First name :");
+                        string fname = Console.ReadLine();
+                        RegistrationValidation Checkpattern = new RegistrationValidation();
+                        Checkpattern.Checkfirstname(fname);
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("invalid option");
+                        break;
+                }
+            }
             Console.ReadLine();
         }
     }
---
+++ UserRegistration/UserRegistration/RegistrationValidation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration
+{
+    internal class RegistrationValidation
+    {
+        public void Checkfirstname(string firstname)
+        {
+            string Firstnamepattern = "^[A-Z][a-z]{2}$";
+            if (Regex.IsMatch(firstname, Firstnamepattern))
+                Console.WriteLine("First Name is valid");
+            else
+                Console.WriteLine("Invalid name!!");
+        }
+    }
+}
